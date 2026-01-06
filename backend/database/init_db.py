9f6451f65de00382046b83e97cc2032925e15c98
ilyassulen <suelen.ilyas@gmail.com>
2026-01-06 11:13:12 +0100

    Add DocumentParse model and update Document model with parses relationship

--- backend/database/init_db.py
+++ backend/database/init_db.py
@@ -1,4 +1,5 @@
 from backend.database.database import engine, Base
 from backend.models.document import Document
+from backend.models.document_parse import DocumentParse
 
 Base.metadata.create_all(engine)
